45a5444c8f5ce1d0a90278fe7fd7343915694980
Jason Mansfield <trentonknight@gmail.com>
2011-02-17 21:42:35 -0500

    skeletal functions.

--- hashTABLE.cpp
+++ hashTABLE.cpp
@@ -2,18 +2,47 @@
 
 using namespace std;
 
-int randNUMS();
+struct TABLE{
+  int key;
+  TABLE* next;
+};
+const int MAX_KEYS = 5000;
+
+int randNUMS(int rand[MAX_KEYS]);
 int hashTableSize();
-void threeHashMethods();
+void threeHashMethods(int rand[MAX_KEYS],int listSIZE);
 int openAddressing_LP();
 int openAddressing_DH();
 int seperateCHAINING();
 int averageHashTable();
 
 int main(){
+  int tableSIZE = 0;
+  int rand[MAX_KEYS] = {0};
 
-
+  randNUMS(rand);
+  tableSIZE = hashTableSize();
+  threeHashMethods(rand,tableSIZE);
 
   return 0;
 }
+int hashTableSize(){
+  int userCHOOSE = 0;
+  ///user must choose above 6500
+  userCHOOSE = 7000; 
+  return userCHOOSE;
+}
+int randNUMS(int rand[MAX_KEYS]){
+  ///temporary fix till hash is running
+  for(int a = 0; a < 10; a++){
+    rand[a] = a;
+  }
+  return *rand;
+}
+void threeHashMethods(int rand[MAX_KEYS],int listSIZE){
+  int address = 0;
+
+  
+}
+
 
