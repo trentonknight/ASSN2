0872b3fb18f8032a74ecae60b326086255c67049
Jason Mansfield <trentonknight@gmail.com>
2011-02-20 03:31:40 -0500

    slef calculating load factor so far.

--- hashTABLE.cpp
+++ hashTABLE.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 using namespace std;
 
@@ -18,9 +19,9 @@ int HASH(int key,int listSIZE);
 void threeHashMethods(int *randARRAY,int tbSIZE);
 int* openAddressing(int *randARRAY,int tbSIZE);
 int seperateCHAINING();
-int quadraticPROBE(int address,int *HASH,int probeTHIS,int loadFACTOR);
+int linearPROBE(int address,int *HASH,int probeTHIS,double loadFACTOR);
 int doubleHASH(int key,int tbSIZE);
-void searchELEMENTS(int *randARRAY,int *HT,int tbSIZE);
+void listSEARCH(int *randARRAY,int *HT,int tbSIZE);
 
 int main(){
   int tbSIZE = 0;
@@ -83,28 +84,28 @@ int randNUMS(int *randARRAY){
 }
 void threeHashMethods(int *randARRAY,int tbSIZE){
   int *HT;
+ 
 
   ///this menu will allow user to select collision method
   HT = openAddressing(randARRAY,tbSIZE);
- 
+  listSEARCH(randARRAY,HT,tbSIZE);
 }
 int* openAddressing(int *randARRAY,int tbSIZE){
   int key = 0,
       address = 0,
       hashTABLE[tbSIZE],
-      *HT = hashTABLE;
-  int loadFACTOR = 0.75 * tbSIZE;
- 
-  for(int a = 0; a < tbSIZE * tbSIZE; a++){
-  hashTABLE[a] = 0;
-  }
-
-  while(randARRAY[key] != 0 && address < loadFACTOR){
+     *HT = hashTABLE;
+  int load = (5000.00 / tbSIZE) * 10;
+  int loadFACTOR = (tbSIZE * load)/10;
+      cout << "5000 items loaded into a " << tbSIZE << " element hash table." << endl;
+      cout << "Load Factor = " << load << endl;
+         
+  while(randARRAY[key] != 0){
     ///get a purposed address
     ///and move through indexes
     ///in array of random int till
     ///empty index is found
-    address = HASH(randARRAY[key],MAX_KEYS);
+    address = HASH(randARRAY[key],loadFACTOR);
     ///if address is available 
     ///grab the key
     if(hashTABLE[address] == 0){
@@ -113,20 +114,42 @@ int* openAddressing(int *randARRAY,int tbSIZE){
     ///if a collision is the result run
     ///a linear probe until available address is found 
     else{
-      address = quadraticPROBE(address,hashTABLE,0,loadFACTOR);
+      address = linearPROBE(address,hashTABLE,0,loadFACTOR);
       hashTABLE[address] = randARRAY[key];
-      cout << address << endl;
       }
     key++;
     }
   return HT;
 }
-int quadraticPROBE(int address,int *HASH,int probeTHIS,int loadFACTOR){
+int linearPROBE(int address,int *HASH,int probeTHIS,double loadFACTOR){
   ///look for empty index but also make sure address does not
   ///exceed the table size.
  while(HASH[address] != probeTHIS && address < loadFACTOR){
-   address = (address^2) + address;
-   ///address = address + 1;
+   address = address + 1;
   }
   return address;
+}
+void listSEARCH(int *randARRAY,int *HT,int tbSIZE){
+  int key = 0,
+      address = 0,
+     examine = 0;
+      double load = (5000.00 / tbSIZE) * 100;
+      load = load / 100;
+      int loadFACTOR = load * tbSIZE;
+
+  while(randARRAY[key] != 0){
+    ///get a purposed address
+    ///and move through indexes
+    ///in array of random int till
+    ///searched int is found
+    address = HASH(randARRAY[key],loadFACTOR);
+   
+    while(HT[address] != randARRAY[key]){
+      address = linearPROBE(address,HT,randARRAY[key],loadFACTOR);
+      examine++;
+      }
+    key++;
+    }
+    cout << "Linear Probing." << endl;
+    cout << examine << " items examined." << endl;
 }
